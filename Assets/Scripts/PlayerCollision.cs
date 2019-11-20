0f394f1affbe17eb666f15f95034f4147b42829e
Daniel <dmarchant@salesforce.com>
2019-11-20 16:49:51 -0500

    adding changes for levels and start

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-    public PlayerMovement movement; 
+    public PlayerMovement movement;
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.tag == "obstacle")
         {
             movement.enabled = false;
+            GameManager gameManager = FindObjectOfType<GameManager>();
+            gameManager.CompleteLevel();
         }        
     }
 
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -26,5 +26,11 @@ public class PlayerMovement : MonoBehaviour
         if( Input.GetKey("a") ) {
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
+
+        if(rb.position.y < -1f )
+        {
+            GameManager gameManager = FindObjectOfType<GameManager>();
+            gameManager.CompleteLevel();
+        }
     }
 }
